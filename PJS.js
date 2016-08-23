3f64f118233a2430be380c693c79bbeb70862192
L.Carquet <laetiticarquet@gmail.com>
2016-08-24 00:55:46 +0200

    create two new attributes that change and delete todos

--- PJS.js
+++ PJS.js
@@ -8,7 +8,15 @@ var todoList = {
 	addTodos: function(todo){
 		this.todos.push(todo);
 		this.displayTodos();
+	},
+	changeTodos: function(position, newValue) {
+		this.todos[position] = newValue;
+		this.displayTodos();
+	},
+	deleteTodos: function(position) {
+		this.todos.splice(position, 1);
+		this.displayTodos();
 	}
 };
 
-todoList.addTodos('item 5');
+todoList.deleteTodos(1);
