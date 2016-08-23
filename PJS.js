9d4573ea80b86aa89102a247a2d28132bfbae280
L.Carquet <laetiticarquet@gmail.com>
2016-08-24 00:51:11 +0200

    create a new attribute with a function that adds todos

--- PJS.js
+++ PJS.js
@@ -4,7 +4,11 @@ var todoList = {
 	todos: ['item 1', 'item 2', 'item 3'],
 	displayTodos: function() {
 		console.log('my todos', this.todos);
+	},
+	addTodos: function(todo){
+		this.todos.push(todo);
+		this.displayTodos();
 	}
 };
 
-todoList.displayTodos();
+todoList.addTodos('item 5');
