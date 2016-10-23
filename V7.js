4ef2611175ea0f371b4780f14d8b5b0189b61b87
L.Carquet <laetiticarquet@gmail.com>
2016-10-23 14:27:29 +0200

    version 7

--- V7.js
+++ V7.js
@@ -1,53 +1,43 @@
-//1. .toggleAll: if everything is true, make everything false
-//2. .toggleAll: otherwise, make everything true
-
 var todosList = {
   todos: [],
   displayTodos: function(){
       if (this.todos.length === 0){
         console.log("your todo list is empty")
-      } else {
+      }else{
         console.log('My todos: ')
         for (var i = 0; i < this.todos.length; i++){
-          console.log(this.todos[i].todoText)
-          //check if .complete is true
           if (this.todos[i].completed === true){
              console.log("(x)", this.todos[i].todoText);
           } else {
             console.log("( )", this.todos[i].todoText)
           }
-        }
       }
+    }
   },
-
-
   addTodo : function(todoText){
     this.todos.push({
-      todoText: todoText, //property
-      completed: false //property
+      todoText: todoText,
+      completed: false
     });
     this.displayTodos();
   },
-
- 
   changeTodo: function(position, todoText){
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-
-
+  deleteTodo: function(position){
+    this.todos.splice(position,1);
+    this.displayTodos();
+  },
+  
+  
   toggleCompleted: function(position){
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
   },
   
-
-  deleteTodo: function(position){
-    this.todos.splice(position,1);
-    this.displayTodos();
-  },
-
+  
   toggleAll: function(){
     var totalTodos = this.todos.length;
     var completedTodos = 0 ;
@@ -62,14 +52,27 @@ var todosList = {
       //then make everything false
       for ( i = 0; i < totalTodos; i++){
         this.todos[i].completed = false;
-      }
-     //CASE 2
-    } else {
+    //CASE 2
+      } 
+    }else {
         for (i = 0; i < totalTodos; i++){
           this.todos[i].completed = true;
         }
     }
     this.displayTodos();
   }
-  
 };
+
+//access to button
+var displayTodos = document.getElementById('displayTodosButton');
+
+
+displayTodos.addEventListener('click', function(){
+  todosList.displayTodos();
+});
+
+var toggleAllButton = document.getElementById("toggleAll");
+
+toggleAllButton.addEventListener('click', function(){
+  todosList.toggleAll();
+});
