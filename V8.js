7510f4b0566ccce6a6ad2307839c34be78f66249
L.Carquet <laetiticarquet@gmail.com>
2016-10-25 13:31:51 +0200

    Version 8: refactoring JS

---
+++ V8.js
@@ -0,0 +1,84 @@
+var todosList = {
+  todos: [],
+  displayTodos: function(){
+      if (this.todos.length === 0){
+        console.log("your todo list is empty")
+      }else{
+        console.log('My todos: ')
+        for (var i = 0; i < this.todos.length; i++){
+          if (this.todos[i].completed === true){
+             console.log("(x)", this.todos[i].todoText);
+          } else {
+            console.log("( )", this.todos[i].todoText)
+          }
+      }
+    }
+  },
+  addTodo : function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText){
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position){
+    this.todos.splice(position,1);
+    this.displayTodos();
+  },
+  
+  
+  toggleCompleted: function(position){
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  
+  
+  toggleAll: function(){
+    var totalTodos = this.todos.length;
+    var completedTodos = 0 ;
+    //get number of completed todos
+    for (var i = 0; i< totalTodos; i++){
+      if (this.todos[i].completed === true){
+        completedTodos++;
+      }
+    }
+    //CASE 1: if everything is true, make everything false.
+    if (completedTodos === totalTodos){
+      //then make everything false
+      for ( i = 0; i < totalTodos; i++){
+        this.todos[i].completed = false;
+    //CASE 2
+      } 
+    }else {
+        for (i = 0; i < totalTodos; i++){
+          this.todos[i].completed = true;
+        }
+    }
+    this.displayTodos();
+  }
+};
+
+//REFACTORING : we want the method of this object to handle whatever is done inside of this object- give values to onclick attr
+var handlers = {
+  displayTodos: function(){
+    todosList.displayTodos();
+  },
+  toggleAll: function(){
+    todosList.toggleAll();
+  }
+};
+
+//V7 before refactoring
+// var displayTodos = document.getElementById('displayTodosButton');
+// var toggleAllButton = document.getElementById("toggleAll");
+// displayTodos.addEventListener('click', function(){
+//   todosList.displayTodos();
+// });
+// toggleAllButton.addEventListener('click', function(){
+//   todosList.toggleAll();
+// });
